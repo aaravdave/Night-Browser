79bef4edbd4baa0d9b1c0fc14945ee3a14623eb7
Aarav Dave <aarinaarav@gmail.com>
2021-02-06 15:35:16 -0500

    Update main.py
    Home Page and Tab system is now working! By changing the tabs dictionary value from 'home' to something else, you will get a blank screen!

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import socket
 from tkinter import *
 
+tabs = {1: 'home'}
+current = 1
+
 window = Tk()
-# Under development.
+window.geometry('1000x600')
+window.title('Night Browser')
+
+if tabs[current] == 'home':
+    url = Entry(window, text='Night', font='/System/Library/Fonts/Avenir.ttc', background='#292929', borderwidth='3px', foreground='white')
+    search = Entry(window, text='Night', font='/System/Library/Fonts/Avenir.ttc', background='#292929', borderwidth='3px', foreground='white')
+
+    url.place(x=100, y=50, width=800)
+    search.place(x=300, y=300, width=400, height=50)
+
+    window.configure(background='#191919')
+
+window.mainloop()
+
